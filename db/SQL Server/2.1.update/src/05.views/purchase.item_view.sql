5d8daddbe4578db3d954774c716724f7b1386909
binodnp@outlook.com <binodnp@outlook.com>
2018-06-19 15:59:35 +0545

    Routine development.

---
+++ db/SQL Server/2.1.update/src/05.views/purchase.item_view.sql
@@ -0,0 +1,44 @@
+﻿IF OBJECT_ID('purchase.item_view') IS NOT NULL
+DROP VIEW purchase.item_view;
+
+GO
+
+
+
+CREATE VIEW purchase.item_view
+AS
+SELECT
+    inventory.items.item_id,
+    inventory.items.item_code,
+    inventory.items.item_name,
+    inventory.items.is_taxable_item,
+    inventory.items.barcode,
+    inventory.items.item_group_id,
+    inventory.item_groups.item_group_name,
+    inventory.item_types.item_type_id,
+    inventory.item_types.item_type_name,
+    inventory.items.brand_id,
+    inventory.brands.brand_name,
+    inventory.items.preferred_supplier_id,
+    inventory.items.unit_id,
+    inventory.get_associated_unit_list_csv(inventory.items.unit_id) AS valid_units,
+    inventory.units.unit_code,
+    inventory.units.unit_name,
+    inventory.items.hot_item,
+    inventory.items.cost_price,
+    inventory.items.cost_price_includes_tax,
+    inventory.items.photo
+FROM inventory.items
+INNER JOIN inventory.item_groups
+ON inventory.item_groups.item_group_id = inventory.items.item_group_id
+INNER JOIN inventory.item_types
+ON inventory.item_types.item_type_id = inventory.items.item_type_id
+LEFT JOIN inventory.brands
+ON inventory.brands.brand_id = inventory.items.brand_id
+INNER JOIN inventory.units
+ON inventory.units.unit_id = inventory.items.unit_id
+WHERE inventory.items.deleted = 0
+AND inventory.items.allow_purchase = 1
+AND inventory.items.maintain_inventory = 1;
+
+GO
--- db/SQL Server/purchase-2.1.update.sql
+++ db/SQL Server/purchase-2.1.update.sql
@@ -1194,7 +1194,7 @@ BEGIN
     
     
 		INSERT INTO purchase.supplier_payments(transaction_master_id, supplier_id, currency_code, amount, er_debit, er_credit, cash_repository_id, posted_date, bank_id, bank_instrument_code, bank_transaction_code)
-		SELECT @transaction_master_id, @supplier_id, @currency_code, @amount,  @exchange_rate_debit, @exchange_rate_credit, @cash_repository_id, @posted_date, @bank_account_id, @bank_instrument_code, @bank_tran_code;
+		SELECT @transaction_master_id, @supplier_id, @currency_code, @amount,  @exchange_rate_debit, @exchange_rate_credit, @cash_repository_id, @posted_date, @bank_id, @bank_instrument_code, @bank_tran_code;
 
 		EXECUTE finance.auto_verify @transaction_master_id, @office_id;
 
@@ -1646,6 +1646,53 @@ GO
 
 
 -->-->-- src/Frapid.Web/Areas/MixERP.Purchases/db/SQL Server/2.1.update/src/03.menus/menus.sql --<--<--
+
+
+-->-->-- src/Frapid.Web/Areas/MixERP.Purchases/db/SQL Server/2.1.update/src/05.views/purchase.item_view.sql --<--<--
+IF OBJECT_ID('purchase.item_view') IS NOT NULL
+DROP VIEW purchase.item_view;
+
+GO
+
+
+
+CREATE VIEW purchase.item_view
+AS
+SELECT
+    inventory.items.item_id,
+    inventory.items.item_code,
+    inventory.items.item_name,
+    inventory.items.is_taxable_item,
+    inventory.items.barcode,
+    inventory.items.item_group_id,
+    inventory.item_groups.item_group_name,
+    inventory.item_types.item_type_id,
+    inventory.item_types.item_type_name,
+    inventory.items.brand_id,
+    inventory.brands.brand_name,
+    inventory.items.preferred_supplier_id,
+    inventory.items.unit_id,
+    inventory.get_associated_unit_list_csv(inventory.items.unit_id) AS valid_units,
+    inventory.units.unit_code,
+    inventory.units.unit_name,
+    inventory.items.hot_item,
+    inventory.items.cost_price,
+    inventory.items.cost_price_includes_tax,
+    inventory.items.photo
+FROM inventory.items
+INNER JOIN inventory.item_groups
+ON inventory.item_groups.item_group_id = inventory.items.item_group_id
+INNER JOIN inventory.item_types
+ON inventory.item_types.item_type_id = inventory.items.item_type_id
+LEFT JOIN inventory.brands
+ON inventory.brands.brand_id = inventory.items.brand_id
+INNER JOIN inventory.units
+ON inventory.units.unit_id = inventory.items.unit_id
+WHERE inventory.items.deleted = 0
+AND inventory.items.allow_purchase = 1
+AND inventory.items.maintain_inventory = 1;
+
+GO
 
 
 -->-->-- src/Frapid.Web/Areas/MixERP.Purchases/db/SQL Server/2.1.update/src/05.views/purchase.purchase_search_view.sql --<--<--
